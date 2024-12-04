8d1525461a5b4f9efeec46ad38c2ac91f2086fae
Rabinder Singh <rab_singh@hotmail.com>
2024-12-04 22:05:03 +0000

    Views done

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    if request.method == "GET":
+       return HttpResponse("This was a GET request")
+    else:
+        return HttpResponse(request.method)
